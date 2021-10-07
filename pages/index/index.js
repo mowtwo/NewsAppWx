b30edaf302baed4210b5cf08d9034b416fbe04eb
mowtwo <1046099511@qq.com>
2021-10-07 15:07:19 +0800

    Initial Commit

---
+++ pages/index/index.js
@@ -0,0 +1,18 @@
+Page({
+  data: {
+    swiperList: [
+      {
+        title: '神舟十三号船箭组合体转运至发射区',
+        image: 'https://rs1.huanqiucdn.cn/dp/api/files/imageDir/11fa491f5922b4dc9dbff2c66556d6f7.jpg?w=600'
+      },
+      {
+        title: '各地迎来返程高峰',
+        image: 'https://rs1.huanqiucdn.cn/dp/api/files/imageDir/72249e994bea85078276295ff76ae368.jpg?w=600'
+      },
+      {
+        title: '阿富汗临时政府恢复发送护照',
+        image: 'https://rs1.huanqiucdn.cn/dp/api/files/imageDir/4d2f1b09900117247eacf11ce82dc4f9.jpg?w=600'
+      }
+    ]
+  }
+})
